d8c56ac92e037e2137bc2eb342891753c31b14fd
Abdurehman05 <abdukr5@gmail.com>
2020-10-02 16:44:49 -0500

    first commit using react tool

---
+++ src/components/customer/CustomerCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Customer.css";
+
+export const CustomerCard = () => (
+  <section className="customer">
+    <h3 className="customer__name">Abdurehman Kass</h3>
+    <div className="customer__address">Address: 3030 Grace Falls Way</div>
+  </section>
+);
---
+++ src/components/employee/EmployeeCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Employee.css";
+
+export const EmployeeCard = () => (
+  <section className="employee">
+    <h3 className="employee__name">Hiba kemal</h3>
+    <div className="company__address">Address: 3030 Washingotn Dr</div>
+  </section>
+);
