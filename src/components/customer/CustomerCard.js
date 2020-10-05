69929dfb3b007f9e518c089c11254e1fd8c36cab
Abdurehman05 <abdukr5@gmail.com>
2020-10-05 13:51:43 -0500

    navBar and Application, customers locations and employees links display their matching resources

--- src/components/customer/CustomerCard.js
+++ src/components/customer/CustomerCard.js
@@ -3,7 +3,7 @@ import "./Customer.css";
 
 export const CustomerCard = () => (
   <section className="customer">
-    <h3 className="customer__name">Abdurehman Kass</h3>
+    <h3 className="customer__name">Abdurehman Kassa</h3>
     <div className="customer__address">Address: 3030 Grace Falls Way</div>
   </section>
 );
--- src/components/employee/EmployeeCard.js
+++ src/components/employee/EmployeeCard.js
@@ -4,6 +4,6 @@ import "./Employee.css";
 export const EmployeeCard = () => (
   <section className="employee">
     <h3 className="employee__name">Hiba kemal</h3>
-    <div className="company__address">Address: 3030 Washingotn Dr</div>
+    <div className="company__address">Address: 6560 Washingotn Dr</div>
   </section>
 );
