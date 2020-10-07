40a17c13f8091947819bbff92f051b05af9250c6
Abdurehman05 <abdukr5@gmail.com>
2020-10-07 10:06:27 -0500

    AnimalForm compnenet added

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -9,6 +9,7 @@ import { AnimalList } from "./animal/AnimalList";
 import { CustomerList } from "./customer/CustomerList";
 import { LocationList } from ".//location/LocationList";
 import { EmployeeList } from "./employee/EmployeeList";
+import { AnimalForm } from "./animal/AnimalForm";
 
 // import { CustomerCard } from "./customer/CustomerCard";
 import { EmployeeCard } from "./employee/EmployeeCard";
@@ -23,24 +24,42 @@ export const ApplicationViews = props => {
       {/* Render the animal list when http://localhost:3000/animals */}
       <AnimalProvider>
         <Route path="/animals">
+          <h2>Animals</h2>
           <AnimalList />
         </Route>
       </AnimalProvider>
+
+      <AnimalProvider>
+        <LocationProvider>
+          <CustomerProvider>
+            <Route exact path="/animals/create">
+              <AnimalForm />
+            </Route>
+          </CustomerProvider>
+        </LocationProvider>
+      </AnimalProvider>
+
       {/* Render the animal list when http://localhost:3000/location */}
+
       <LocationProvider>
         <Route path="/locations">
+          <h2>Locations</h2>
           <LocationList />
         </Route>
       </LocationProvider>
       {/* Render the animal list when http://localhost:3000/customers */}
+
       <CustomerProvider>
         <Route path="/customers">
+          <h2>Customers</h2>
           <CustomerList />
         </Route>
       </CustomerProvider>
       {/* Render the animal list when http://localhost:3000/employees */}
+
       <EmployeeProvider>
         <Route path="/employees">
+          <h2>Employees</h2>
           <EmployeeList />
         </Route>
       </EmployeeProvider>
